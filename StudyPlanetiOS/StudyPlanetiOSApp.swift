fe6995f62ad77d4e8bc8e062bec572e9e18c08f7
jorisvanduyse <jorisduyse@gmail.com>
2023-12-18 15:16:26 +0100

    Initial Commit

---
+++ StudyPlanetiOS/StudyPlanetiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyPlanetiOSApp.swift
+//  StudyPlanetiOS
+//
+//  Created by Joris Van Duyse on 18/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct StudyPlanetiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
