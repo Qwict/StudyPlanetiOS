36b3b23ef574450655dc6c0f3f2673f4f4184529
jorisvanduyse <jorisduyse@gmail.com>
2023-12-28 21:46:43 +0100

    v0.5.1 -> Implemented Empty DiscoveredPlanetsView screen

--- StudyPlanet/StudyPlanet/Presentation/Screens/DiscoveredPlanetsView.swift
+++ StudyPlanet/StudyPlanet/Presentation/Screens/DiscoveredPlanetsView.swift
@@ -11,11 +11,31 @@ struct DiscoveredPlanetsView: View {
 //            Text("My Discovered Planets")
 //                    .font(.title)
 //                    .fontWeight(.bold)
-            List(MockData.planets) { planet in
-                NavigationLink(destination: TimeSelectionView(planet: planet)) {
-                    PlanetListItem(planet: planet)
+            if MockData.planets.isEmpty {
+                Image(
+                        systemName: "questionmark.circle"
+                )
+                        .resizable()
+                        .foregroundColor(.secondary)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 90, height: 90)
+                Text("No Planets discovered yet.")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.center)
+                Text("Discover new planets in the Discover tab.")
+                        .multilineTextAlignment(.center)
+            } else {
+                VStack {
+                    List(MockData.planets) { planet in
+                        NavigationLink(destination: TimeSelectionView(planet: planet)) {
+                            PlanetListItem(planet: planet)
+                        }
+                    }
                 }
             }
+
+
         }
     }
 }
